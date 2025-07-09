4f800859377e872bfae9fba2fc27ef3b01661fd1
namnvhe <namlegend586@gmail.com>
2025-07-10 00:54:00 +0700

    all_cahng

---
+++ MenMaxAPI/MenMaxBackEnd/MenMaxBackEnd/Models/Cart.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MenMaxBackEnd.Models;
+
+public partial class Cart
+{
+    public int Id { get; set; }
+
+    public int Count { get; set; }
+
+    public int? ProductId { get; set; }
+
+    public string? UserId { get; set; }
+
+    public virtual Product? Product { get; set; }
+
+    public virtual User? User { get; set; }
+}
+
+
